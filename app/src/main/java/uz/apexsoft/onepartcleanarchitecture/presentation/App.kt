d01a84846050f8f4e2d3abdc265aa5bb67606f8d
Dostonjon-98 <mahmanazarovdoston98@gmail.com>
2024-11-04 15:21:31 +0500

    delete oncreate() method of App class

--- app/src/main/java/uz/apexsoft/onepartcleanarchitecture/presentation/App.kt
+++ app/src/main/java/uz/apexsoft/onepartcleanarchitecture/presentation/App.kt
@@ -2,8 +2,4 @@ package uz.apexsoft.onepartcleanarchitecture.presentation
 
 import android.app.Application
 
-class App:Application(){
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
+class App:Application()
